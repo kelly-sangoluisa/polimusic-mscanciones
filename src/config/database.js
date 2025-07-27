5ccf937e4097c0524bfb5946408d016588c60c48
kelly-sangoluisa <kelly.sangoluisa@epn.edu.ec>
2025-07-26 22:49:51 -0500

    refactor: agregar documentacion

--- src/config/database.js
+++ src/config/database.js
@@ -1,29 +1,3 @@
-/*const sql = require('mssql');
-require('dotenv').config();
-
-const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_NAME,
-    port: parseInt(process.env.DB_PORT),
-    options: {
-        encrypt: false,
-        trustServerCertificate: true
-    }
-};
-
-async function connectDB() {
-    try {
-        const pool = await sql.connect(config);
-        console.log('✅ Conectado a SQL Server');
-        return pool;
-    } catch (err) {
-        console.error('❌ Error al conectar a la base de datos:', err);
-    }
-}
-
-module.exports = { connectDB, sql };*/
 const sql = require('mssql');
 require('dotenv').config();
 
